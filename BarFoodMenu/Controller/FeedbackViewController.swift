ce9832d43162e9caad9d57089e8bd634c4bfbbea
iOSDeveloper <schoolboy0719@gmail.com>
2020-09-29 22:12:41 +0300

    git init

---
+++ BarFoodMenu/Controller/FeedbackViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FavoriteViewController.swift
+//  BarFoodMenu
+//
+//  Created by Baby on 29.09.2020.
+//  Copyright © 2020 bar. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackViewController: UIViewController {
+
+    @IBOutlet weak var feedbackProductName: UILabel!
+    @IBOutlet weak var feedbackAddBtn: UIButton!
+    @IBOutlet weak var feedbacksTV: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BarFoodMenu/View/FeedbackTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteTableViewCell.swift
+//  BarFoodMenu
+//
+//  Created by Baby on 29.09.2020.
+//  Copyright © 2020 bar. All rights reserved.
+//
+
+import UIKit
+import Cosmos
+
+class FeedbackTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var feedbackUserPhoto: UIImageView!
+    @IBOutlet weak var feedbackStar: CosmosView!
+    @IBOutlet weak var feedbackUserName: UILabel!
+    @IBOutlet weak var feedbackDate: UILabel!
+    @IBOutlet weak var feedbackContent: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
